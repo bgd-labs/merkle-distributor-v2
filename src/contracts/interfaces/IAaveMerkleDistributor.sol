e71b87fbe4a01be68a6d93e5b96104d40384826b
Lukas <lukasstrassel@googlemail.com>
2024-04-04 11:01:05 +0200

    feat: add rescuable (#3)

--- src/contracts/interfaces/IAaveMerkleDistributor.sol
+++ src/contracts/interfaces/IAaveMerkleDistributor.sol
@@ -84,21 +84,4 @@ interface IAaveMerkleDistributor {
    * the token
    */
   function addDistributions(address[] memory tokens, bytes32[] memory merkleRoots) external;
-
-  /**
-   * @dev transfer ERC20 from the utility contract, for ERC20 recovery in case of stuck tokens due
-   * direct transfers to the contract address.
-   * @param erc20Token erc20 token to transfer
-   * @param to recipient of the transfer
-   * @param amount amount to send
-   */
-  function emergencyTokenTransfer(address erc20Token, address to, uint256 amount) external;
-
-  /**
-   * @dev transfer native Ether from the utility contract, for native Ether recovery in case of stuck Ether
-   * due selfdestructs or transfer ether to pre-computated contract address before deployment.
-   * @param to recipient of the transfer
-   * @param amount amount to send
-   */
-  function emergencyEtherTransfer(address to, uint256 amount) external;
 }
